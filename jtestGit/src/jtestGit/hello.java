36a1b52efcc6fdecbfd2da9880f136cf8cf99195
doychin doychev <doychin.doychev@gmail.com>
2014-01-21 15:47:12 +0000

    msg

---
+++ jtestGit/src/jtestGit/hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jtestGit;
+
+/**
+ * @author segaty
+ *
+ */
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
